036b14bf92a03b3fb14ae262ba4e03d0c1c12a71
ZibinRoman <61138127+ZibinRoman@users.noreply.github.com>
2020-04-10 11:15:03 +0500

    Add files via upload

---
+++ src/main/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package main;
+
+import graphicaluserinterface.GraphicalUserInterface;
+
+/**
+ * @author Roman
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		initialize();
+	}
+	
+	private static void initialize() {
+		// TODO Auto-generated method stub
+		GraphicalUserInterface.start();
+	}
+}
